b4a81ae8f840f4451aab39030b2d59dcb5cc4541
ElijahLenhard <elijahlenhard@gmail.com>
2022-07-03 17:57:10 -0400

    Updated the package structure to include the com package

---
+++ com/company/CharPoly.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class CharPoly {
+
+    double lambda;
+    double real;
+
+
+    public CharPoly(double lambda, double real){
+        this.lambda =lambda;
+        this.real= real;
+    }
+
+
+
+
+}
